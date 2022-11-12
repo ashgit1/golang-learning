44549c022ffac20fcef9d2b3f1363b2be98f321f
Ashishkumar Gupta <adoreashish@gmail.com>
2022-11-12 19:07:30 +0530

    Initial commit on pointers

---
+++ 07mypointers/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Welcome to a class on pointers")
+
+	var ptr *int
+	fmt.Println("Value of pointers is ", ptr)
+
+	myNumber := 23
+	var ptr2 = &myNumber
+	fmt.Println("Value of the pointer is ", ptr2)
+	fmt.Println("Actual Value of the pointer is ", *ptr2)
+
+	*ptr2 = *ptr2 + 2
+	fmt.Println("New Value of the pointer is ", myNumber)
+
+}
